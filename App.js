fda245df41ae8adddbe59b487154e9d723480837
FIRST_NAME LAST_NAMEsak1340 <sak_rosak-123@hotmail.com>
2018-10-02 17:48:12 +0700

    THREE

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+  render() {
+    return (
+      <div>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+        }
+      </div>
+    );
+  }
 }
 export default App;
