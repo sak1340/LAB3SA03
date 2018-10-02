ae25a6da49e0f95b99331eaf38b4f78e75ee10ac
FIRST_NAME LAST_NAMEsak1340 <sak_rosak-123@hotmail.com>
2018-10-02 17:57:03 +0700

    FIVE

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className = "App">
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-        }
+      <div>
+        <WordCard value="hello" />
       </div>
     );
   }
 }
-
-export default App;
+  export default App;
